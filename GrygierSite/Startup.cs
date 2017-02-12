27884aabf04da151d73d0891acb8c5512b4b5e46
Marcin Grygierczyk <kwaska1000@gmail.com>
2017-02-12 19:36:40 +0100

    Init

---
+++ GrygierSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GrygierSite.Startup))]
+namespace GrygierSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
