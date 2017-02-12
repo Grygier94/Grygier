2b437a932c46230b6eef1fb285a07a8f041137b9
Marcin Grygierczyk <kwaska1000@gmail.com>
2017-02-12 20:06:19 +0100

    Add products and categories table.

---
+++ GrygierSite/Migrations/201702121904072_CreateProductTable.cs
@@ -0,0 +1,45 @@
+namespace GrygierSite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateProductTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        ParentCategoryId = c.Byte(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        Description = c.String(nullable: false, maxLength: 3000),
+                        DateOfIssue = c.DateTime(nullable: false),
+                        LastUpdate = c.DateTime(nullable: false),
+                        Category_Id = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.Category_Id, cascadeDelete: true)
+                .Index(t => t.Category_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "Category_Id" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ GrygierSite/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GrygierSite.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(3000)]
+        public string Description { get; set; }
+
+        public DateTime DateOfIssue { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+    }
+}
